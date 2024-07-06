00587ec7e575ddbf6c15ce8f9201e7daab9b56a9
Luffy <luffydbl@outlook.com>
2024-07-06 21:02:47 +0800

    理论算法 完善

--- uniform_distribution.py
+++ uniform_distribution.py
@@ -1,19 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import random
 BEGIN = 0
 END = 1
 distribuition = [BEGIN,1,1,1,1,1,
                  1,1,1,1,1,END]
 
-count=100
+
 M=[]
-
-
+PERIOD=10
+sub=np.zeros(10)
+sub=( [-1,0,0,0,0,0,0,0,0,1],
+      [1,-1,0,0,0,0,0,0,0,0],
+      [0,1,-1,0,0,0,0,0,0,0],
+      [0,0,1,-1,0,0,0,0,0,0],
+      [0,0,0,1,-1,0,0,0,0,0],
+      [0,0,0,0,1,-1,0,0,0,0],
+      [0,0,0,0,0,1,-1,0,0,0],
+      [0,0,0,0,0,0,1,-1,0,0],
+      [0,0,0,0,0,0,0,1,-1,0],
+      [0,0,0,0,0,0,0,0,1,-1]  )
 
 
 
 def show():
+    count=100
     while count:
         print(distribuition)
         for i in range(1,11):
@@ -24,10 +35,7 @@ def show():
     plt.xlim(0,1)
     plt.show()
 
-
-if __name__ == '__main__':
-    # show()
-    # print(M)
+def generate_matrix():
     for i in range(10):
         Mi = np.identity(10)
         Mi[i][i]=0
@@ -35,15 +43,42 @@ if __name__ == '__main__':
         Mi[(i+1)%10][i]=0.5
         M.append(Mi)
 
-    for i in range(10):
-        print("第"+str(i+1)+"个矩阵")
-        print(M[i])
-        print("\n")
+    # for i in range(10):
+    #     print("第"+str(i+1)+"个矩阵")
+    #     print(M[i])
+    #     print("\n")
 
-    I_10 = np.identity(10)
-    print(I_10)
+def move(array):
     for i in range(10):
-        I_10=np.dot(I_10,M[i])    
-        print("第"+str(i+1)+"个矩阵")
-        print(I_10)
-        print("\n")
+        array[(i+1)%10]+=10
+        array=np.dot(array,M[i])
+        array[(i+1)%10]-=10
+        # print(array)
+    return array
+    
+        
+if __name__ == '__main__':
+    # show()
+    generate_matrix()
+    I_10 = np.identity(10)
+    # print(I_10)
+
+    list=[10,10,10,10,10,
+          10,10,10,10,10]
+    
+
+    array=np.array(list)
+    
+    # exit() 
+    main_count=0
+    while(main_count<30):
+        array=move(array)
+        for i in range(0,10):
+            # plt.plot(array[i]%10,main_count,'o',markersize=1)
+            temp=np.dot(array,sub)
+            plt.plot(temp[i]%10,main_count,'o',markersize=1)
+            print(np.dot(array,sub))
+        main_count+=1
+    # plt.xlim(0,10)
+    plt.show()  
+    
